ec584a302a89500dc9483dee0853c51893b73e0c
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-09-12 11:32:48 -0500

    Bug fix for "Unknown Artist" showing in Table View

--- Amperfy/Api/Ampache/AmpacheXmlServerApi.swift
+++ Amperfy/Api/Ampache/AmpacheXmlServerApi.swift
@@ -102,7 +102,7 @@ class AmpacheXmlServerApi {
 
     func requestArtists(parserDelegate: XMLParserDelegate, startIndex: Int, pollCount: Int = maxItemCountToPollAtOnce) {
         reauthenticateIfNeccessary()
-        if let hostname = credentials?.serverUrl, let auth = authHandshake, pollCount < auth.artistCount {
+        if let hostname = credentials?.serverUrl, let auth = authHandshake, startIndex < auth.artistCount {
             let urlPath = "\(hostname)/server/xml.server.php?auth=\(auth.token)&action=artists&offset=\(startIndex)&limit=\(pollCount)"
             request(fromUrlString: urlPath, viaXmlParser: parserDelegate)
         }
