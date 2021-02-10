442be13862805e4199792e2513afbe342da508fd
Maximilian Bauer <bauer.maximilian@web.de>
2021-02-10 15:17:56 +0100

    PlaylistsVC: adding search bar

--- Amperfy/Screens/ViewController/PlaylistsVC.swift
+++ Amperfy/Screens/ViewController/PlaylistsVC.swift
@@ -3,19 +3,44 @@ import UIKit
 class PlaylistsVC: UITableViewController {
 
     var appDelegate: AppDelegate!
-    var playlists = [Playlist]()
+    var playlistsUnfiltered: [Playlist]!
+    var playlistsFiltered: [Playlist]!
     
+    private let searchController = UISearchController(searchResultsController: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+        
+        configureSearchController()
         tableView.register(nibName: PlaylistTableCell.typeName)
         tableView.rowHeight = PlaylistTableCell.rowHeight
         self.refreshControl?.addTarget(self, action: #selector(PlaylistsVC.handleRefresh), for: UIControl.Event.valueChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        playlists = appDelegate.library.getPlaylists().sortAlphabeticallyAscending()
-        tableView.reloadData()
+        playlistsUnfiltered = appDelegate.library.getPlaylists().sortAlphabeticallyAscending()
+        updateSearchResults(for: searchController)
+    }
+    
+    private func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+
+        if #available(iOS 11.0, *) {
+            // For iOS 11 and later, place the search bar in the navigation bar.
+            navigationItem.searchController = searchController
+            // Make the search bar always visible.
+            navigationItem.hidesSearchBarWhenScrolling = false
+        } else {
+            // For iOS 10 and earlier, place the search controller's search bar in the table view's header.
+            tableView.tableHeaderView = searchController.searchBar
+        }
+        
+        searchController.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self // Monitor when the search button is tapped.
+        self.definesPresentationContext = true
     }
     
     // MARK: - Table view data source
@@ -25,30 +50,31 @@ class PlaylistsVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return playlists.count
+        return playlistsFiltered.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: PlaylistTableCell = dequeueCell(for: tableView, at: indexPath)
         
-        let playlist = playlists[indexPath.row]
+        let playlist = playlistsFiltered[indexPath.row]
         cell.display(playlist: playlist)
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let playlist = playlists[indexPath.row]
+        let playlist = playlistsFiltered[indexPath.row]
         performSegue(withIdentifier: Segues.toPlaylistDetail.rawValue, sender: playlist)
     }
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let playlist = playlists[indexPath.row]
-            playlists.remove(at: indexPath.row)
+            let playlist = playlistsFiltered[indexPath.row]
+            playlistsFiltered.remove(at: indexPath.row)
             appDelegate.persistentLibraryStorage.deletePlaylist(playlist)
             appDelegate.persistentLibraryStorage.saveContext()
+            playlistsUnfiltered = appDelegate.library.getPlaylists().sortAlphabeticallyAscending()
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
@@ -69,11 +95,39 @@ class PlaylistsVC: UITableViewController {
             
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
-                self.playlists = self.appDelegate.library.getPlaylists()
-                self.tableView.reloadData()
+                self.playlistsUnfiltered = self.appDelegate.library.getPlaylists().sortAlphabeticallyAscending()
+                self.updateSearchResults(for: self.searchController)
                 refreshControl.endRefreshing()
             }
         }
     }
     
+}
+
+extension PlaylistsVC: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            playlistsFiltered = playlistsUnfiltered.filterBy(searchText: searchText)
+        } else {
+            playlistsFiltered = playlistsUnfiltered
+        }
+        tableView.reloadData()
+    }
+    
+}
+
+extension PlaylistsVC: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        updateSearchResults(for: searchController)
+    }
+    
+}
+
+extension PlaylistsVC: UISearchControllerDelegate {
 }
