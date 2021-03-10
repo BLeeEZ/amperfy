cb29cb342cd7949f7c2cba995dbdf3d32ebd9644
Maximilian Bauer <bauer.maximilian@web.de>
2021-03-10 20:38:56 +0100

    LibraryVC: change to static table view

--- Amperfy/Screens/ViewController/LibraryVC.swift
+++ Amperfy/Screens/ViewController/LibraryVC.swift
@@ -1,10 +1,23 @@
 import Foundation
 import UIKit
 
-class LibraryVC: UIViewController {
+class LibraryVC: UITableViewController {
+    
+    @IBOutlet weak var latestSongAsteriskLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureAsteriskStyle()
     }
     
+    func configureAsteriskStyle() {
+        latestSongAsteriskLabel.layer.cornerRadius = 6.5
+        latestSongAsteriskLabel.layer.masksToBounds = true
+        latestSongAsteriskLabel.backgroundColor = UIColor.defaultBlue
+        if #available(iOS 13.0, *) {
+            latestSongAsteriskLabel.textColor = UIColor.secondarySystemBackground
+        } else {
+            latestSongAsteriskLabel.textColor = UIColor.white
+        }
+    }
 }
