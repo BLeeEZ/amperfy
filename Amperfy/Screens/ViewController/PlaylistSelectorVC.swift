87429a287e7bde363020fe1261780ce65b894f5a
Maximilian Bauer <bauer.maximilian@web.de>
2021-03-15 21:13:34 +0100

    PlaylistSelectorVC: add search bar

--- Amperfy/Screens/ViewController/PlaylistSelectorVC.swift
+++ Amperfy/Screens/ViewController/PlaylistSelectorVC.swift
@@ -6,13 +6,17 @@ class PlaylistSelectorVC: UITableViewController {
     var songsToAdd: [Song]?
     private var appDelegate: AppDelegate!
     private var playlistsAsyncFetch = AsynchronousFetch(result: nil)
-    private var playlists = [Playlist]()
+    private var playlistsUnfiltered = [Playlist]()
+    private var playlistsFiltered = [Playlist]()
     
+    private let searchController = UISearchController(searchResultsController: nil)
     private let loadingSpinner = SpinnerViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+
+        configureSearchController()
         tableView.register(nibName: PlaylistTableCell.typeName)
         tableView.rowHeight = PlaylistTableCell.rowHeight
         
@@ -23,31 +27,49 @@ class PlaylistSelectorVC: UITableViewController {
                 self.appDelegate.storage.persistentContainer.performBackgroundTask() { (context) in
                     let backgroundLibrary = LibraryStorage(context: context)
                     self.playlistsAsyncFetch = backgroundLibrary.getPlaylistsAsync(forMainContex: self.appDelegate.storage.context) { playlistsResult in
-                        self.playlists = playlistsResult.sortAlphabeticallyAscending()
+                        self.playlistsUnfiltered = playlistsResult.sortAlphabeticallyAscending()
+                        self.updateSearchResults(for: self.searchController)
                         self.loadingSpinner.hide()
-                        self.tableView.reloadData()
                     }
                 }
                 
             }
             tableView.tableHeaderView?.addSubview(newPlaylistTableHeaderView)
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+
         loadingSpinner.display(on: self)
         appDelegate.storage.persistentContainer.performBackgroundTask() { (context) in
             let backgroundLibrary = LibraryStorage(context: context)
             self.playlistsAsyncFetch = backgroundLibrary.getPlaylistsAsync(forMainContex: self.appDelegate.storage.context) { playlistsResult in
-                self.playlists = playlistsResult.sortAlphabeticallyAscending()
+                self.playlistsUnfiltered = playlistsResult.sortAlphabeticallyAscending()
+                self.updateSearchResults(for: self.searchController)
                 self.loadingSpinner.hide()
-                self.tableView.reloadData()
             }
         }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         self.playlistsAsyncFetch.cancle()
+    }
+    
+    private func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+
+        if #available(iOS 11.0, *) {
+            // For iOS 11 and later, place the search bar in the navigation bar.
+            navigationItem.searchController = searchController
+            // Make the search bar always visible.
+            navigationItem.hidesSearchBarWhenScrolling = false
+        } else {
+            // For iOS 10 and earlier, place the search controller's search bar in the table view's header.
+            tableView.tableHeaderView = searchController.searchBar
+        }
+        
+        searchController.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self // Monitor when the search button is tapped.
+        self.definesPresentationContext = true
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -65,20 +87,20 @@ class PlaylistSelectorVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return playlists.count
+        return playlistsFiltered.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: PlaylistTableCell = dequeueCell(for: tableView, at: indexPath)
         
-        let playlist = playlists[indexPath.row]
+        let playlist = playlistsFiltered[indexPath.row]
         cell.display(playlist: playlist)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let playlist = playlists[indexPath.row]
+        let playlist = playlistsFiltered[indexPath.row]
         if let song = songToAdd {
             playlist.append(song: song)
         }
@@ -88,4 +110,38 @@ class PlaylistSelectorVC: UITableViewController {
         dismiss()
     }
     
+}
+
+extension PlaylistSelectorVC: UISearchResultsUpdating {
+    
+    func filterSearchResults(for searchController: UISearchController, playlists: [Playlist]) -> [Playlist]  {
+        var filteredPlaylists = [Playlist]()
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            filteredPlaylists = playlists.filterBy(searchText: searchText)
+        } else {
+            filteredPlaylists = playlists
+        }
+        return filteredPlaylists
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        playlistsFiltered = filterSearchResults(for: searchController, playlists: playlistsUnfiltered)
+        tableView.reloadData()
+    }
+    
+}
+
+extension PlaylistSelectorVC: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        updateSearchResults(for: searchController)
+    }
+
+}
+
+extension PlaylistSelectorVC: UISearchControllerDelegate {
 }
