63b1b2200c3fac023fb3ac1647038088ee334e95
Maximilian Bauer <bauer.maximilian@web.de>
2021-08-04 21:28:20 +0200

    Attributions: adding CoreDataMigration by William Boles

--- Amperfy/Screens/ViewController/LicenseVC.swift
+++ Amperfy/Screens/ViewController/LicenseVC.swift
@@ -61,9 +61,8 @@ class LicenseVC: UIViewController {
          <p>__________________________________</p>
 
          <h3>LNPopupController</h3>
-         <p>Copyright (c) 2015 Leo Natan</p>
-
          <p>The MIT License (MIT)</p>
+         <p>Copyright (c) 2015 Leo Natan</p>
 
          <p>Permission is hereby granted, free of charge, to any person obtaining a copy
          of this software and associated documentation files (the "Software"), to deal
@@ -86,6 +85,7 @@ class LicenseVC: UIViewController {
          <p>__________________________________</p>
 
          <h3>MarqueeLabel</h3>
+         <p>The MIT License (MIT)</p>
          <p>Copyright (c) 2011-2017 Charles Powell</p>
              
          <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
@@ -104,6 +104,7 @@ class LicenseVC: UIViewController {
          <p>__________________________________</p>
 
          <h3>NotificationBanner</h3>
+         <p>The MIT License (MIT)</p>
          <p>Copyright (c) 2017-2018 Daltron &lt;daltonhint4@gmail.com&gt;</p>
 
          <p>Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -123,6 +124,30 @@ class LicenseVC: UIViewController {
          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
          THE SOFTWARE.</p>
+
+         <p>__________________________________</p>
+
+         <h3>CoreDataMigrationRevised-Example</h3>
+         <p>MIT License</p>
+         <p>Copyright (c) 2017 William Boles</p>
+
+         <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+         of this software and associated documentation files (the "Software"), to deal
+         in the Software without restriction, including without limitation the rights
+         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+         copies of the Software, and to permit persons to whom the Software is
+         furnished to do so, subject to the following conditions:</p>
+
+         <p>The above copyright notice and this permission notice shall be included in all
+         copies or substantial portions of the Software.</p>
+
+         <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+         IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+         FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+         SOFTWARE.</p>
 
          <p>__________________________________</p>
 
