2d283abba078cb2e3693e0eb005316865e1aa053
Maximilian Bauer <maximilian.bauer@web.de>
2021-09-07 20:42:18 +0200

    Player: increase playtime slider touch area for thumb

--- Amperfy/Screens/Basics/PlaytimeSlider.swift
+++ Amperfy/Screens/Basics/PlaytimeSlider.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class PlaytimeSlider: UISlider {
     
+    private var thumbTouchSize = CGSize(width: 50, height: 30)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -10,6 +12,22 @@ class PlaytimeSlider: UISlider {
         let customBounds = CGRect(origin: bounds.origin, size: CGSize(width: bounds.size.width, height: 4.0))
         super.trackRect(forBounds: customBounds)
         return customBounds
+    }
+
+    // MARK: - Increase touch area for thumb
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let increasedBounds = bounds.insetBy(dx: -thumbTouchSize.width, dy: -thumbTouchSize.height)
+        let containsPoint = increasedBounds.contains(point)
+        return containsPoint
+    }
+
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let percentage = CGFloat((value - minimumValue) / (maximumValue - minimumValue))
+        let thumbSizeHeight = thumbRect(forBounds: bounds, trackRect:trackRect(forBounds: bounds), value:0).size.height
+        let thumbPosition = thumbSizeHeight + (percentage * (bounds.size.width - (2 * thumbSizeHeight)))
+        let touchLocation = touch.location(in: self)
+        return touchLocation.x <= (thumbPosition + thumbTouchSize.width) && touchLocation.x >= (thumbPosition - thumbTouchSize.width)
     }
 
 }
