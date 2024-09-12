82401e9d6142a637e1504887494193561ef4a9e8
SchlaubiSchlump <>
2024-09-12 15:07:26 +0200

    Update AppKitIntegration bundle

---
+++ AppKitIntegration/AppKitIntegration.h
@@ -0,0 +1,19 @@
+//
+//  AppKitIntegration.h
+//  AppKitIntegration
+//
+//  Created by David Klopp on 12.09.24.
+//  Copyright © 2024 Maximilian Bauer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppKitIntegration.
+FOUNDATION_EXPORT double AppKitIntegrationVersionNumber;
+
+//! Project version string for AppKitIntegration.
+FOUNDATION_EXPORT const unsigned char AppKitIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppKitIntegration/PublicHeader.h>
+
+
