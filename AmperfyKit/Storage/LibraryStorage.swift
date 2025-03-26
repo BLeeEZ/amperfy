0bb526228a4c54f8ece8385ebe4e6a4f6db38f50
Maximilian Bauer <bauer.maximilian@web.de>
2025-03-26 23:04:10 +0100

    LibraryStorage: saveContext not calling fatalError on error

--- AmperfyKit/Storage/LibraryStorage.swift
+++ AmperfyKit/Storage/LibraryStorage.swift
@@ -2276,7 +2276,15 @@ public class LibraryStorage: PlayableFileCachable {
         // Replace this implementation with code to handle the error appropriately.
         // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         let nserror = error as NSError
-        fatalError("Unresolved error \(nserror), \(nserror.localizedDescription)")
+        os_log(
+          "CoreData Save Context Error: %s",
+          log: log,
+          type: .error,
+          error.localizedDescription
+        )
+        #if DEBUG
+          fatalError("Unresolved error \(nserror), \(nserror.localizedDescription)")
+        #endif
       }
     }
   }
